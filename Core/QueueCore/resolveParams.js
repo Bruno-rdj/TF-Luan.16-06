3baf675dae18b6f87700ef29560f088f1f820960
bruno <brunorochaj250705@gmail.com>
2025-06-16 20:01:13 -0300

    Correção do worker para processar a fila do-task-queue com concorrência 3

--- Core/QueueCore/resolveParams.js
+++ Core/QueueCore/resolveParams.js
@@ -1,7 +1,7 @@
 import minimist from "minimist";
 
 /** Obter a fila e a concorrencia que o worker irá se conectar 
- * 1. Irá priorizar o parametro de terminal --queue
+ * 1. Irá priorizar o parametro de terminal posicional
  * 2. Irá buscar a variável de ambiente RABBITMQ_QUEUE
  * 3. Irá conectar na fila default
  */
@@ -12,11 +12,15 @@ export default () => {
      */
     const [, , ...rawArgs] = process.argv;
     const args = minimist(rawArgs);
-    delete args["_"];
-
-    const queue = (args["queue"]) ? (args["queue"]) : (process.env.RABBITMQ_QUEUE || "default");
-
-    const concurrency = (args["concurrency"]) ? (args["concurrency"]) : (process.env.RABBITMQ_CONCURRENCY || 1);
+    
+    // Argumentos posicionais
+    const posArgs = args["_"] || [];
+    
+    // Primeiro argumento posicional é a fila
+    const queue = posArgs[0] || process.env.RABBITMQ_QUEUE || "default";
+    
+    // Segundo argumento posicional é a concorrência
+    const concurrency = posArgs[1] || process.env.RABBITMQ_CONCURRENCY || 1;
 
     return {
         queue: queue,
